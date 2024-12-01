c2b5de65f8dffd2ae0500f36462e5fa1183f0f8c
jr72s <82790621+jr72s@users.noreply.github.com>
2024-11-30 22:33:55 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,9 @@
 import { Label } from './types.js';
+export const DELETE = '3lc7nwys3xz2k';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: 3lc7nwynxac2m,
+    rkey: '3lc7nwynxac2m',
     identifier: 'Rightwing Propaganda',
     locales: [
       { lang: 'en', name: 'Rightwing Propaganda', description: 'Labels posts with links to foxnews.com, newsmax.com, nypost.com, oann.com, washingtonexaminer.com, theepochtimes.com, theblaze.com, dailywire.com, dailycaller.com, westernjournal.com, nationalreview.com, washingtontimes.com, townhall.com, thegatewaypundit.com, breitbart.com, redstate.com, hannity.com, thefederalist.com, dailysignal.com, pjmedia.com. twitchy.com, wnd.com, lifesitenews.com'}

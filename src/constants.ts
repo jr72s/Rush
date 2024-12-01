eee9782a0414c0e235870937b2b2719652dbc2d9
jr72s <82790621+jr72s@users.noreply.github.com>
2024-11-30 22:29:10 -0500

    Update constants.ts
    also removed delete rkey

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,9 @@
 import { Label } from './types.js';
-
-export const DELETE = 'insert-rkey-of-delete-post-here';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'earth',
+    rkey: 3lc7nwynxac2m,
+    identifier: 'Rightwing Propaganda',
     locales: [
       { lang: 'en', name: 'Rightwing Propaganda', description: 'Labels posts with links to foxnews.com, newsmax.com, nypost.com, oann.com, washingtonexaminer.com, theepochtimes.com, theblaze.com, dailywire.com, dailycaller.com, westernjournal.com, nationalreview.com, washingtontimes.com, townhall.com, thegatewaypundit.com, breitbart.com, redstate.com, hannity.com, thefederalist.com, dailysignal.com, pjmedia.com. twitchy.com, wnd.com, lifesitenews.com'}
     ]

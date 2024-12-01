dd339a6f38d0d9f8a65d93b7cbdb08bfe7ad794d
jr72s <82790621+jr72s@users.noreply.github.com>
2024-12-01 09:40:04 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ export const LABELS: Label[] = [
         lang: 'en',
         name: 'Rightwing Propaganda',
         description:
-          'Labels posts with links to foxnews.com, newsmax.com, nypost.com, oann.com, washingtonexaminer.com, theepochtimes.com, theblaze.com, dailywire.com, dailycaller.com, westernjournal.com, nationalreview.com, washingtontimes.com, townhall.com, thegatewaypundit.com, breitbart.com, redstate.com, hannity.com, thefederalist.com, dailysignal.com, pjmedia.com. twitchy.com, wnd.com, lifesitenews.com',
+          'Labels posts with links to foxnews.com, newsmax.com, nypost.com, oann.com, washingtonexaminer.com, theepochtimes.com, dailymail.co.uk, theblaze.com, dailywire.com, dailycaller.com, westernjournal.com, nationalreview.com, washingtontimes.com, townhall.com, thegatewaypundit.com, breitbart.com, redstate.com, hannity.com, thefederalist.com, dailysignal.com, pjmedia.com. twitchy.com, wnd.com, lifesitenews.com',
       },
     ],
   },
@@ -26,6 +26,7 @@ const URL_DOMAINS = [
   'oann.com',
   'washingtonexaminer.com',
   'theepochtimes.com',
+  'dailymail.co.uk'
   'theblaze.com',
   'dailywire.com',
   'dailycaller.com',
